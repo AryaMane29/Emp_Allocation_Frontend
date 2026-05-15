9df117b5af3b55a969d4814b39d3cf3affb28e30
AryaMane29 <aaryamane2929@gmail.com>
2026-05-15 14:27:54 +0530

    Update API URL to production

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://localhost:7189/api';
+const BASE_URL = 'https://empallocationapi-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
